679fa35f45aa832d5c55926f1ff3d1dbf7326e39
drobking <egor.drobotenko@gmail.com>
2020-12-09 15:34:51 +0200

    1

---
+++ Olx_Proj/ClientApp/src/components/CategoryMenu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class CategoryMenu extends Component{
+render(){
+    return(
+<div className="d-flex justify-content-center">
+
+<div className="col-9 bg-secondary">
+
+</div>
+</div>
+
+    )}
+
+}
